2e98a96af6639fbdf67ac369c2edcb98b5bb5d6e
sandygriuella <152032067+Aurtikent@users.noreply.github.com>
2025-05-10 08:10:01 +0530

    addded option to view title

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ def get_top_10( url, number):
 
 
 
-def get_link(id):
+def get_link(id, title=False):
     end_point = f'https://hacker-news.firebaseio.com/v0/item/{id}.json'
     req_object = requests.get(end_point)
     id_dict = eval(req_object.content.decode())
     id_url = id_dict['url']
 
+    if title == True:
+        description = id_dict['title']
+        
+        return id_url, description
+    
+    
     return id_url
 
 
@@ -24,14 +30,21 @@ url = ' https://hacker-news.firebaseio.com/v0/topstories.json'
 no_of_items_to_fetch = int(input("No of top news stories to fetch: "))
 stories_id = get_top_10(url, no_of_items_to_fetch)
 
+
+
 print(stories_id)
 
 for i in stories_id:
     try:
-        print(get_link(i))
-    except: # Need some Error Handling and what to do when such things creep on the top page of Hackernews.
+        
+        link = get_link(i, title=False)
+        print(link)
+        # print("Description: " ,link[1])
+        # print()e
+    except:
+        # print()
+#     except: # Need some Error Handling and what to do when such things creep on the top page of Hackernews.
         print("id = ", i)
-# get_link(43942318)
 
 
 
